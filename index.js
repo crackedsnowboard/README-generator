304488e5a322a269c8499d76613eff53649105d7
Prince of Kochi <mathenjs@gmail.com>
2020-07-09 22:10:53 -0700

    added questions for readme generator

--- index.js
+++ index.js
@@ -2,7 +2,51 @@ const inquirer = require("inquirer");
 const util = require("inquirer");
 const fs = require("fs")
 
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the Project Title?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the description?",
+            name: "name"
 
+        },
+        {
+            type: "input",
+            message: "What is the installation?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the usage?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is the liscense?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Contributing?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Tests?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Questions to add?",
+            name: "name"
+        }
+
+    ])
 
 const questions = [
 
